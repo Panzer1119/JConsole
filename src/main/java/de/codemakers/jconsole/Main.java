e5d20b22475ef1e6b233abf5567ea396a8354e9c
Paul Hagedorn <panzer1119@gmail.com>
2018-04-20 19:13:24 +0200

    Added nogui argument
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/jconsole/Main.java
+++ src/main/java/de/codemakers/jconsole/Main.java
@@ -3,7 +3,21 @@ package de.codemakers.jconsole;
 public class Main {
     
     public static final void main(String[] args) {
+        boolean nogui = false;
+        if (args.length == 1) {
+            if (args[0].startsWith("-")) {
+                args[0] = args[0].substring(1);
+            }
+            if (args[0].equalsIgnoreCase("nogui")) {
+                nogui = true;
+            }
+        }
         System.out.println("Hi!");
+        if (nogui) {
+            System.out.println("nogui");
+        } else {
+            System.out.println("!nogui");
+        }
     }
     
 }
